af987a50f8334a2b1fb69149c9c626d543e7403c
Broofdeath <Broofdeath@LAPTOP-J2TF8SCO>
2018-05-29 15:41:19 -0400

    Confirmar

---
+++ WebSite/App_Code/Pais.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Pais
+/// </summary>
+public class Pais
+{
+    public Pais()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
